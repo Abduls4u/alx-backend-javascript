033be6beaa273329eca879f1f4b68eaf9091acc2
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-10 21:34:32 +0000

    Edit task 10 solution

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, val] of array.entries()) {
-    array[idx] = appendString + val;
+  for (let keys of array.entries()) {
+    array[keys[0]] = appendString + keys[1];
   }
 
   return array;
