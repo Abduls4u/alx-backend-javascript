7348a1306f5732fab3408a5bb98759069b0de0c9
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-16 21:22:14 +0000

    Fix error in 2 and 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(thePromise) {
-  return (
-    thePromise
+  return thePromise
       .then(() => ({ status: 200, body: 'Success' }))
       .catch(() => Error())
       .finally(() => { console.log('Got a response from the API'); })
-  );
 }
