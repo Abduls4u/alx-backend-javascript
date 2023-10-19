953a41e1b6f8a221747857fd70f89c7a05515fbf
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-19 13:07:33 +0000

    Fix task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
-export default function handleResponseFromAPI(thePromise) {
-  const obj = { body: 'Success', status: 200 };
-  return thePromise
-    .then(() => obj)
+/* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
