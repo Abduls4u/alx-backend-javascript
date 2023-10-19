5eef4087ea03a9390cf9bc198b1837e93500b4de
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-19 13:43:08 +0000

    Fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
-/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 const uPhoto = uploadPhoto();
 const cUser = createUser();
 
 export default function handleProfileSignup() {
-  return Promise.all([cUser, uPhoto])
-    .then((values) => {
-      console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`);
-    })
-    .catch(() => { console.log('Signup system offline'); })
+  return Promise.all([cUser, uPhoto]).then((values) => {
+    console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
